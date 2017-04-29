87e62285400cb564c6362ae5555b81ef81141980
gernest <geofreyernest@live.com>
2017-04-29 13:17:26 +0300

    first cut

--- layout.go
+++ layout.go
@@ -1,17 +1,94 @@
 package layout
 
 import (
+	"context"
+
+	"honnef.co/go/js/dom"
+
 	"github.com/gopherjs/vecty"
 	"github.com/gopherjs/vecty/elem"
+	"github.com/gopherjs/vecty/event"
 	"github.com/gopherjs/vecty/prop"
 )
 
+const (
+	SmallScreenOnly = "mdl-layout--small-screen-only"
+	LargeScreenOnly = "mdl-layout--large-screen-only"
+)
+
+var layoutTabUpdates chan string
+var doc dom.Document
+
+func init() {
+	layoutTabUpdates = make(chan string)
+	doc = dom.GetWindow().Document()
+}
+
+func switchState(id string, active bool) {
+	toActive(id+"-bar", active)
+	toActive(id, active)
+}
+
+func toActive(id string, state bool) {
+	e := doc.GetElementByID(id)
+	a := "is-active"
+	if state {
+		e.Class().Add(a)
+	} else {
+		e.Class().Remove(a)
+	}
+}
+
 type Layout struct {
 	vecty.Core
-	IsJS     bool
-	Header   *Header
-	Children vecty.MarkupOrComponentOrHTML
-	Content  vecty.MarkupOrComponentOrHTML
+	IsJS              bool
+	Header            *Header
+	Children          vecty.MarkupOrComponentOrHTML
+	Content           vecty.MarkupOrComponentOrHTML
+	FixedDrawer       bool
+	FixedHeader       bool
+	NoDrawerBtn       bool
+	NoDestopDrawerBtn bool
+	LargeScreenOnly   bool
+	TabView           bool
+	Panes             []*Panel
+	cancel            func()
+	activePane        string
+}
+
+func New(tabview bool) *Layout {
+	l := &Layout{TabView: tabview}
+	if tabview {
+		ctx, cancel := context.WithCancel(context.Background())
+		l.cancel = cancel
+		return l.watch(ctx)
+	}
+	return l
+}
+
+func (l *Layout) watch(ctx context.Context) *Layout {
+	go func() {
+	done:
+		for {
+			select {
+			case id := <-layoutTabUpdates:
+				if id != l.activePane {
+					switchState(l.activePane, false)
+					switchState(id, true)
+					l.activePane = id
+				}
+			case <-ctx.Done():
+				break done
+			}
+		}
+	}()
+	return l
+}
+
+func (l *Layout) Unmount() {
+	if l.cancel != nil {
+		l.cancel()
+	}
 }
 
 func (l *Layout) Render() *vecty.HTML {
@@ -20,12 +97,49 @@ func (l *Layout) Render() *vecty.HTML {
 	if l.IsJS {
 		c["mdl-js-layout"] = true
 	}
+	if l.FixedDrawer {
+		c["mdl-layout--fixed-drawer"] = true
+	}
+	if l.FixedHeader {
+		c["mdl-layout--fixed-header"] = true
+	}
+	if l.NoDrawerBtn {
+		c["mdl-layout--no-drawer-button"] = true
+	}
+	if l.NoDestopDrawerBtn {
+		c["mdl-layout--no-desktop-drawer-button"] = true
+	}
 	var p vecty.List
 	if l.Header != nil {
 		p = append(p, l.Header)
 	}
-	if l.Content != nil {
-		p = append(p, elem.Main(prop.Class("mdl-layout__content"), l.Content))
+
+	if l.LargeScreenOnly {
+		c["mdl-layout--large-screen-only"] = true
+	}
+	if l.TabView {
+		b := &TabBar{}
+		var pt vecty.List
+		for i := 0; i < len(l.Panes); i++ {
+			b.Links = append(b.Links, &TabLink{
+				ID:       l.Panes[i].ID,
+				Text:     l.Panes[i].Name,
+				IsActive: l.Panes[i].IsActive,
+			})
+			pt = append(pt, l.Panes[i])
+		}
+		if l.Header == nil {
+			l.Header = &Header{}
+		}
+		l.Header.TabView = true
+		l.Header.TabNav = b
+		p = append(p, elem.Main(
+			pt,
+		))
+	} else {
+		if l.Content != nil {
+			p = append(p, elem.Main(prop.Class("mdl-layout__content"), l.Content))
+		}
 	}
 	return elem.Div(
 		l.Children,
@@ -36,9 +150,17 @@ func (l *Layout) Render() *vecty.HTML {
 
 type Header struct {
 	vecty.Core
-	Icon     *Icon
-	Rows     []*HeaderRow
-	Children vecty.MarkupOrComponentOrHTML
+	Icon             *Icon
+	Row              *HeaderRow
+	Children         vecty.MarkupOrComponentOrHTML
+	Scroll           bool
+	Waterfall        bool
+	WaterfallHideTop bool
+	Transparent      bool
+	Seamed           bool
+	TabView          bool
+	TabNav           *TabBar
+	ManualTabSwitch  bool
 }
 
 func (h *Header) Render() *vecty.HTML {
@@ -46,13 +168,35 @@ func (h *Header) Render() *vecty.HTML {
 	if h.Icon != nil {
 		l = append(l, h.Icon)
 	}
-	for i := 0; i < len(h.Rows); i++ {
-		l = append(l, h.Rows[i])
+	c := make(vecty.ClassMap)
+	c["mdl-layout__header"] = true
+	if h.Scroll {
+		c["mdl-layout__header--scroll"] = true
+	}
+	if h.Waterfall {
+		c["mdl-layout__header--waterfall"] = true
+	}
+	if h.WaterfallHideTop {
+		c["mdl-layout__header--waterfall-hide-top"] = true
+	}
+	if h.Transparent {
+		c["mdl-layout__header--transparent"] = true
+	}
+	if h.Seamed {
+		c["mdl-layout__header--seamed"] = true
+	}
+	if h.TabView {
+		return elem.Header(
+			h.Children,
+			c,
+			h.Row,
+			h.TabNav,
+		)
 	}
 	return elem.Header(
 		h.Children,
-		prop.Class("mdl-layout__header"),
-		l,
+		c,
+		h.Row,
 	)
 
 }
@@ -137,5 +281,66 @@ func (t *Title) Render() *vecty.HTML {
 		t.Children,
 		prop.Class("mdl-layout-title"),
 		vecty.Text(t.Text),
+	)
+}
+
+type TabBar struct {
+	vecty.Core
+	Links  []*TabLink
+	Manual bool
+}
+
+type TabLink struct {
+	vecty.Core
+	ID       string
+	IsActive bool
+	Text     string
+	Children vecty.MarkupOrComponentOrHTML
+}
+
+func (t *TabLink) Render() *vecty.HTML {
+	c := make(vecty.ClassMap)
+	c["mdl-layout__tab"] = true
+	if t.IsActive {
+		c["is-active"] = true
+	}
+	return elem.Anchor(
+		t.Children,
+		prop.ID("#"+t.ID),
+		prop.ID(t.ID+"-bar"), c,
+		vecty.Text(t.Text),
+		event.Click(func(e *vecty.Event) {
+			go func() {
+				layoutTabUpdates <- t.ID
+			}()
+		}),
+	)
+}
+
+type TabNav struct {
+	vecty.Core
+	TabBar   *TabBar
+	Tabs     []*Panel
+	Children vecty.MarkupOrComponentOrHTML
+}
+
+type Panel struct {
+	vecty.Core
+	ID       string
+	Name     string
+	IsActive bool
+	Children vecty.MarkupOrComponentOrHTML
+}
+
+func (t *Panel) Render() *vecty.HTML {
+	c := make(vecty.ClassMap)
+	c["mdl-layout__tab-panel"] = true
+	if t.IsActive {
+		c["is-active"] = true
+	}
+	return elem.Section(
+		c,
+		prop.ID(t.ID),
+		t.Children,
 	)
 }
